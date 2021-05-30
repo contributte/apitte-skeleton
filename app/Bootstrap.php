d76cc91ced6ce02cffea6ab88d11579d956be959
Jiri Travnicek <jiri.travnicek@outlook.com>
2021-05-31 01:59:43 +0200

    bootstrap changed to class @see nette/sandbox

---
+++ app/Bootstrap.php
@@ -0,0 +1,50 @@
+<?php declare(strict_types=1);
+
+namespace App;
+
+use Contributte\Bootstrap\ExtraConfigurator;
+use Nette\Configurator;
+use Nette\DI\Compiler;
+use Tracy\Debugger;
+
+class Bootstrap
+{
+	public static function boot(): Configurator
+	{
+		$configurator = new ExtraConfigurator();
+		$configurator->setTempDirectory(__DIR__ . '/../temp');
+
+		// Disable default extensions
+		unset($configurator->defaultExtensions['security']);
+
+		$configurator->onCompile[] = function (ExtraConfigurator $configurator, Compiler $compiler): void {
+			// Add env variables to config structure
+			$compiler->addConfig(['parameters' => $configurator->getEnvironmentParameters()]);
+		};
+
+		// According to NETTE_DEBUG env
+		$configurator->setEnvDebugMode();
+
+		// Enable tracy and configure it
+		$configurator->enableTracy(__DIR__ . '/../log');
+		Debugger::$errorTemplate = __DIR__ . '/resources/tracy/500.txt';
+
+		// Provide some parameters
+		$configurator->addParameters([
+			'rootDir' => realpath(__DIR__ . '/..'),
+			'appDir' => __DIR__,
+			'wwwDir' => realpath(__DIR__ . '/../www'),
+		]);
+
+		// Load development or production config
+		if (getenv('NETTE_ENV', true) === 'dev') {
+			$configurator->addConfig(__DIR__ . '/config/env/dev.neon');
+		} else {
+			$configurator->addConfig(__DIR__ . '/config/env/prod.neon');
+		}
+
+		$configurator->addConfig(__DIR__ . '/config/config.local.neon');
+
+		return $configurator;
+	}
+}
