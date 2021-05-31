4274ba7bef0a1cfc8de11577a8db62e1f3327cdf
Jiri Travnicek <jiri.travnicek@outlook.com>
2021-05-31 02:00:12 +0200

    remove maintenance, it doesn't exist

--- www/index.php
+++ www/index.php
@@ -1,9 +1,5 @@
 <?php declare(strict_types = 1);
 
-// Uncomment this line if you must temporarily take down your site for maintenance.
-//require 'maintenance.php';
-//die();
-
 require __DIR__ . '/../vendor/autoload.php';
 
 // Let bootstrap create Dependency Injection container.
