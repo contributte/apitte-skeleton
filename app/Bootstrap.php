7ad00df95d27b19f9fbd82b46fedbacd4910aa47
Petr Parolek <petr.parolek@gmail.com>
2022-01-25 20:27:43 +0100

    Update composer & codesniffer (#369)

--- app/Bootstrap.php
+++ app/Bootstrap.php
@@ -1,4 +1,4 @@
-<?php declare(strict_types=1);
+<?php declare(strict_types = 1);
 
 namespace App;
 
@@ -9,6 +9,7 @@ use Tracy\Debugger;
 
 class Bootstrap
 {
+
 	public static function boot(): Configurator
 	{
 		$configurator = new ExtraConfigurator();
@@ -47,4 +48,5 @@ class Bootstrap
 
 		return $configurator;
 	}
+
 }
